54c522fe23e072f95b65bc6001a4a75593f221ef
Juanda4555 <caldon9.8.06@gmail.com>
2025-04-16 19:01:38 -0500

    elementos de la clase BTreePag

--- src/Clases/BTreePag.java
+++ src/Clases/BTreePag.java
@@ -1,6 +1,61 @@
-
 package Clases;
 
 public class BTreePag {
+
+    private int elements;
+    private BTreePag child[];
+    private int key[];
+    private boolean leaf;
+    private BTreePag father;
+
+    public BTreePag(BTreePag father) {
+        this.father = father;
+        this.elements = 0;
+        this.child = new BTreePag[5];
+        this.key = new int[5];
+        this.leaf = true;
+    }
+
+    public int getElements() {
+        return elements;
+    }
+
+    public void setElements(int elements) {
+        this.elements = elements;
+    }
+
+    public BTreePag[] getChild() {
+        return child;
+    }
+
+    public void setChild(BTreePag[] child) {
+        this.child = child;
+    }
+
+    public int[] getKey() {
+        return key;
+    }
+
+    public void setKey(int[] key) {
+        this.key = key;
+    }
+
+    public boolean isLeaf() {
+        return leaf;
+    }
+
+    public void setLeaf(boolean leaf) {
+        this.leaf = leaf;
+    }
+
+    public BTreePag getFather() {
+        return father;
+    }
+
+    public void setFather(BTreePag father) {
+        this.father = father;
+    }
     
+    
+
 }
